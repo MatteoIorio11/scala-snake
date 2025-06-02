aa6bb0f1442a37ffc6cf88aeada2706923ea0dcf
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-06-02 14:22:12 +0200

    feat: add Direction

--- src/main/scala/core/entity/Direction.scala
+++ src/main/scala/core/entity/Direction.scala
@@ -1,1 +1,8 @@
-package core.entity class Direction
+package core.entity
+
+enum Direction:
+  case UP, DOWN, LEFT, RIGHT
+
+  def isOpposite(other: Direction): Boolean = (this, other) match
+    case (UP, DOWN) | (DOWN, UP) | (LEFT, RIGHT) | (RIGHT, LEFT) => true
+    case _ => false
