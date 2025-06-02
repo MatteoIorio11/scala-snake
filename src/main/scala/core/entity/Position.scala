0961f35a585a9c3122c5538f944157045609cd79
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-06-02 14:22:23 +0200

    feat: add Position

--- src/main/scala/core/entity/Position.scala
+++ src/main/scala/core/entity/Position.scala
@@ -1,1 +1,12 @@
-package core.entity class Position
+package core.entity
+
+case class Position(x: Int, y: Int) {
+  def translate(direction: Direction): Position = direction match {
+    case Direction.UP    => Position(x, y - 1)
+    case Direction.DOWN  => Position(x, y + 1)
+    case Direction.LEFT  => Position(x - 1, y)
+    case Direction.RIGHT => Position(x + 1, y)
+    case _ => throw new IllegalArgumentException("Invalid direction")
+  }
+}
+
